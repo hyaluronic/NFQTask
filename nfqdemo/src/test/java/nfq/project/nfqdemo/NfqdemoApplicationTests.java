d731a9fef31c10d48cbbd356abc3020c56dd6768
hyaluronic <60736298+hyaluronic@users.noreply.github.com>
2021-02-18 17:40:19 +0200

    Added project template

---
+++ nfqdemo/src/test/java/nfq/project/nfqdemo/NfqdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package nfq.project.nfqdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NfqdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
