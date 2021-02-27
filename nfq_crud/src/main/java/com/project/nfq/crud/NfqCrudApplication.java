7a8602defc73d1a503349e545ad9d9e4de26c821
hyaluronic <60736298+hyaluronic@users.noreply.github.com>
2021-02-27 17:11:33 +0200

    Added project template and database

---
+++ nfq_crud/src/main/java/com/project/nfq/crud/NfqCrudApplication.java
@@ -0,0 +1,13 @@
+package com.project.nfq.crud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NfqCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NfqCrudApplication.class, args);
+	}
+
+}
