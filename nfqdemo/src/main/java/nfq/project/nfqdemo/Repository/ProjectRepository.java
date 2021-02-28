20f69a9358b37f745db554bbcbe2fbc298656289
hyaluronic <60736298+hyaluronic@users.noreply.github.com>
2021-02-28 21:05:58 +0200

    Fix: deleted unnecessary project folder

--- nfqdemo/src/main/java/nfq/project/nfqdemo/Repository/ProjectRepository.java
+++
@@ -1,8 +0,0 @@
-package nfq.project.nfqdemo.Repository;
-
-import nfq.project.nfqdemo.Domain.Entities.Project;
-import org.springframework.data.repository.CrudRepository;
-
-
-public interface ProjectRepository extends CrudRepository<Project, Integer> {
-}
--- nfqdemo/src/main/java/nfq/project/nfqdemo/Repository/TeacherRepository.java
+++
@@ -1,7 +0,0 @@
-package nfq.project.nfqdemo.Repository;
-
-import nfq.project.nfqdemo.Domain.Entities.Teacher;
-import org.springframework.data.repository.CrudRepository;
-
-public interface TeacherRepository extends CrudRepository<Teacher, Integer> {
-}
